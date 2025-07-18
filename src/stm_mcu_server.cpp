17d6c3daea259ec119d94ad05ed1a2fdce9e5b55
fedddot <idanrommm@gmail.com>
2025-07-18 17:00:50 +0000

    isr updated

--- src/stm_mcu_server.cpp
+++ src/stm_mcu_server.cpp
@@ -1,7 +1,17 @@
+
+// Forward declarations
 extern "C" {
     int main(void);
     void Reset_Handler(void);
+    extern unsigned long _estack; // Defined in linker script
 }
+
+typedef void (*isr_procedure_t)(void);
+
+__attribute__((section(".isr_vector"), used)) isr_procedure_t g_pfnVectors[] = {
+    (void (*)(void))(&_estack),   // Initial stack pointer
+    Reset_Handler,                // Reset vector
+};
 
 void Reset_Handler(void) {
     main();
