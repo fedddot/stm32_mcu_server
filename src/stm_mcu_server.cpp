82ec3d4d9af308cd6a9433ed4f0a2a18637dfcb0
fedddot <idanrommm@gmail.com>
2025-07-18 17:06:50 +0000

    code optimized

--- src/stm_mcu_server.cpp
+++ src/stm_mcu_server.cpp
@@ -1,16 +1,14 @@
-
-// Forward declarations
 extern "C" {
     int main(void);
     void Reset_Handler(void);
-    extern unsigned long _estack; // Defined in linker script
+    extern unsigned long _estack;
 }
 
 typedef void (*isr_procedure_t)(void);
 
 __attribute__((section(".isr_vector"), used)) isr_procedure_t g_pfnVectors[] = {
-    (void (*)(void))(&_estack),   // Initial stack pointer
-    Reset_Handler,                // Reset vector
+    (isr_procedure_t)(&_estack),
+    Reset_Handler,
 };
 
 void Reset_Handler(void) {
