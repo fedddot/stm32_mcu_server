394fa62822f3e39d0bfd42151de98717306fd2b5
fedddot <idanrommm@gmail.com>
2025-07-18 14:42:39 +0000

    updated

--- src/stm_mcu_server.cpp
+++ src/stm_mcu_server.cpp
@@ -4,18 +4,21 @@
 #include "ipc_data.hpp"
 #include "ipc_data_writer.hpp"
 #include "ipc_instance.hpp"
+#include "relay_controller.hpp"
 #include "stm32f1xx_hal.h"
 #include "stm32f1xx_hal_gpio.h"
 #include "main.h"
 
+#include "temperature_sensor_controller.hpp"
 #include "thermostat_host_builder.hpp"
-#
+#include "timer_scheduler.hpp"
 #include "proto_thermostat_api_request_parser.hpp"
 #include "proto_thermostat_api_response_serializer.hpp"
 
 using namespace host;
 using namespace vendor;
 using namespace ipc;
+using namespace manager;
 
 static void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
@@ -38,23 +41,49 @@ public:
     }
 };
 
+class StmRelayController: public RelayController {
+public:
+    void set_relay_state(const bool state) override {
+
+    }
+};
+
+class StmTimerScheduler: public TimerScheduler {
+public:
+    manager::Instance<TaskGuard> schedule_task(const Task& task, const std::size_t period_ms) override {
+        throw std::runtime_error("NOT IMPLEMENTED");
+    }
+};
+
+class StmTempSensor: public TemperatureSensorController {
+public:
+    double read_temperature() const {
+        return 29.0;
+    }
+};
+
 int main(void) {
     HAL_Init();
     SystemClock_Config();
     MX_GPIO_Init();
-    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
     
     ThermostatHostBuilder builder;
     builder
         .set_api_request_parser(ApiRequestParser())
         .set_api_response_serializer(ApiResponseSerializer())
         .set_raw_data_reader(ThermostatHostBuilder::RawDataReaderInstance(new RawDataReader()))
-        .set_raw_data_writer(ThermostatHostBuilder::RawDataWriterInstance(new RawDataWriter()));
+        .set_raw_data_writer(ThermostatHostBuilder::RawDataWriterInstance(new RawDataWriter()))
+        .set_relay_controller(ThermostatHostBuilder::RelayControllerInstance(new StmRelayController()))
+        .set_scheduler(ThermostatHostBuilder::SchedulerInstance(new StmTimerScheduler()))
+        .set_temp_sensor(ThermostatHostBuilder::SensorInstance(new StmTempSensor()));
+
     
-    const auto host = builder.build();
+    auto host = builder.build();
     
     while (1) {
-        ;
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+        host.run_once();
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
     }
 }
 
