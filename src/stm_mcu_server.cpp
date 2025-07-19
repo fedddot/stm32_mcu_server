674b7e817d22fb97eb87c52b86a87ccc851b5200
fedddot <idanrommm@gmail.com>
2025-07-19 16:07:28 +0000

    addresses imported

--- src/stm_mcu_server.cpp
+++ src/stm_mcu_server.cpp
@@ -1,16 +1,19 @@
+#include <cstddef>
 #include <cstdint>
 
 extern "C" {
     int main(void);
     void Reset_Handler(void);
     extern std::uint32_t _estack;
-    extern std::uint32_t __data_start;
+    extern volatile std::uint32_t __data_start;
+    extern std::uint32_t end;
+    extern std::uint32_t __rodata_start;
 }
 
 typedef void (*isr_procedure_t)(void);
 
 __attribute__((section(".isr_vector"), used)) isr_procedure_t g_pfnVectors[2] = {
-    (isr_procedure_t)(&_estack),
+    *(isr_procedure_t *)(&_estack),
     Reset_Handler,
 };
 
@@ -19,8 +22,6 @@ void Reset_Handler(void) {
 }
 
 int main(void) {
-    const std::uint32_t jajaja = 0xdeadbeef;
-    const std::uint8_t *data_start_ptr = (std::uint8_t *)__data_start;
     while (1) {
     }
 }
