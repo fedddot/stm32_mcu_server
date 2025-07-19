e4f14d87fcb00cfd6c96456d20b2ac33b816c255
fedddot <idanrommm@gmail.com>
2025-07-19 19:15:43 +0000

    updated

--- src/stm_mcu_server.cpp
+++ src/stm_mcu_server.cpp
@@ -1,20 +1,13 @@
-#include <cstddef>
 #include <cstdint>
 #include <cstring>
 #include <stdexcept>
-#include "proto_thermostat_api_request_parser.hpp"
-#include "proto_thermostat_api_response_serializer.hpp"
 
 extern "C" {
     int main(void);
     void Reset_Handler(void);
-    extern std::uint32_t g_stack_start;
-    extern std::uint32_t g_data_start_ram;
-    extern std::uint32_t g_data_size;
-    extern std::uint32_t g_data_flash_addr;
-    extern std::uint32_t g_tdata_start_ram;
-    extern std::uint32_t g_tdata_end_ram;
-    extern std::uint32_t g_tdata_flash_addr;
+    extern std::uint32_t _data_start_ram;
+    extern std::uint32_t _data_size;
+    extern std::uint32_t _data_flash_addr;
 }
 
 typedef void (*isr_procedure_t)(void);
@@ -25,19 +18,18 @@ __attribute__((section(".isr_vector"), used)) std::uint32_t g_pfnVectors[68] = {
 };
 
 void Reset_Handler(void) {
-    const auto source_ptr = (const void *)g_data_flash_addr;
-    auto dest_ptr = (void *)g_data_start_ram;
-    auto size = g_data_size;
-    if (nullptr == std::memcpy(dest_ptr, source_ptr, size)) {
-        throw std::runtime_error("failed to init .data section");
+    const auto source_ptr = (const char *)_data_flash_addr;
+    auto dest_ptr = (char *)_data_start_ram;
+    auto size = _data_size;
+    while (size) {
+        *dest_ptr = *source_ptr;
+        --size;
     }
-
     main();
 }
 
 int main(void) {
-    const auto parser = ipc::ApiRequestParser();
-    const auto serializer = ipc::ApiResponseSerializer();
+    const auto str_var = std::string("asdf");
     while (1) {
     }
 }
