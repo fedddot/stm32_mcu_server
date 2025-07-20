4ba953db37e8f3d3cbeb98347a58a239782563cf
fedddot <idanrommm@gmail.com>
2025-07-20 18:48:51 +0000

    init_clock added

--- src/mcu_startup.cpp
+++ src/mcu_startup.cpp
@@ -2,6 +2,9 @@
 #include <cstring>
 #include <stdexcept>
 
+#include "stm32f1xx_hal_rcc.h"
+#include "stm32f1xx_hal_flash.h"
+
 #define VECTORS_TABLE_SIZE 68UL
 
 extern "C" {
@@ -15,6 +18,7 @@ extern "C" {
 }
 
 extern int main(void);
+static void init_clock(void);
 
 void reset_isr(void) {
     auto flash_source_ptr = (const char *)&_sidata;
@@ -24,6 +28,7 @@ void reset_isr(void) {
     if (nullptr == std::memcpy(ram_start_ptr, flash_source_ptr, size)) {
         throw std::runtime_error("failed to init .data section");
     }
+    init_clock();
     main();
 }
 
@@ -45,3 +50,26 @@ __attribute__((section(".isr_vector"), used)) std::uint32_t g_pfnVectors[VECTORS
     (std::uint32_t)(&nmi_isr),
     (std::uint32_t)(&hard_fault_isr),
 };
+
+void init_clock(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        hard_fault_isr();
+    }
+
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+        hard_fault_isr();
+    }
+}
