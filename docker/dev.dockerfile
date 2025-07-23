06a59d06000df90fcad164ccd66cad010269c405
fedddot <idanrommm@gmail.com>
2025-07-23 21:56:24 +0000

    updated

--- docker/dev.dockerfile
+++ docker/dev.dockerfile
@@ -1,25 +1,19 @@
 FROM mcu-server-dev:latest
 
-# pico-sdk deps
 RUN apt install -y gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib gdb-multiarch
+RUN apt-get install -y bsdmainutils
+RUN apt install -y python3-pip
+RUN pip3 install --break-system-packages protobuf grpcio-tools
 
-# install externals
 ARG EXTERNAL_PATH=/usr/src/external
 WORKDIR ${EXTERNAL_PATH} 
 
-RUN git clone --branch=main https://github.com/fedddot/mcu_server.git mcu_server
-ENV MCU_SERVER_PATH=${EXTERNAL_PATH}/mcu_server
-
 RUN git clone --branch=v1.8.6 https://github.com/STMicroelectronics/STM32CubeF1.git stm32_cube_f1
 RUN cd stm32_cube_f1 && git submodule update --init --recursive
-ENV STM32_CUBE_F1_SRC_PATH=${EXTERNAL_PATH}/stm32_cube_f1
+ENV STM32_CUBE_SRC_PATH=${EXTERNAL_PATH}/stm32_cube_f1
 
-RUN git clone --branch=0.4.9 https://github.com/nanopb/nanopb.git nanopb
-ENV NANOPB_SRC_PATH=${EXTERNAL_PATH}/nanopb
-
-RUN apt install -y python3-pip
-RUN pip3 install --break-system-packages protobuf grpcio-tools
-RUN apt-get install -y bsdmainutils
+RUN git clone --branch=dev/optimize-footprint-for-mbedded-systems https://github.com/fedddot/mcu_server.git mcu_server
+ENV MCU_SERVER_PATH=${EXTERNAL_PATH}/mcu_server
 
 # server sources should be mapped to this path during container run
 WORKDIR /usr/src/app
--- src/stm_mcu_server.cpp
+++ src/stm_mcu_server.cpp
@@ -5,13 +5,10 @@
 #include "ipc_data.hpp"
 #include "ipc_data_writer.hpp"
 #include "ipc_instance.hpp"
-#include "relay_controller.hpp"
-
-#include "temperature_sensor_controller.hpp"
+#include "thermostat_manager_controller.hpp"
 #include "thermostat_api_request.hpp"
 #include "thermostat_api_response.hpp"
 #include "thermostat_host_builder.hpp"
-#include "timer_scheduler.hpp"
 #include "proto_thermostat_api_request_parser.hpp"
 #include "proto_thermostat_api_response_serializer.hpp"
 
@@ -20,10 +17,23 @@ using namespace vendor;
 using namespace ipc;
 using namespace manager;
 
-static Host<ThermostatVendorApiRequest, ThermostatVendorApiResponse> create_host(void);
+static Host<ThermostatVendorApiRequest, ThermostatVendorApiResponse> create_host(ThermostatManagerController *controller_ptr);
+class StmThermoManagerController: public ThermostatManagerController {
+public:
+    double read_temperature() const override {
+        throw std::runtime_error("NOT IMPLEMENTED");
+    }
+    void set_relay_state(const bool state) override {
+        throw std::runtime_error("NOT IMPLEMENTED");
+    }
+    manager::Instance<TaskGuard> schedule_task(const Task& task, const std::size_t period_ms) override {
+        throw std::runtime_error("NOT IMPLEMENTED");
+    }
+};
 
 int main(void) {
-    auto host = create_host();
+    StmThermoManagerController controller;
+    auto host = create_host(&controller);
     while (1) {
         // host.run_once();
     }
@@ -43,38 +53,13 @@ public:
     }
 };
 
-class StmRelayController: public RelayController {
-public:
-    void set_relay_state(const bool state) override {
-
-    }
-};
-
-class StmTimerScheduler: public TimerScheduler {
-public:
-    manager::Instance<TaskGuard> schedule_task(const Task& task, const std::size_t period_ms) override {
-        throw std::runtime_error("NOT IMPLEMENTED");
-    }
-};
-
-class StmTempSensor: public TemperatureSensorController {
-public:
-    double read_temperature() const {
-        return 29.0;
-    }
-};
-
-ThermostatHostBuilder s_builder;
-inline Host<ThermostatVendorApiRequest, ThermostatVendorApiResponse> create_host(void) {
-    ApiRequestParser s_parser;
-    ApiResponseSerializer s_serializer;
-    s_builder
-        .set_api_request_parser(s_parser)
-        .set_api_response_serializer(s_serializer)
+inline Host<ThermostatVendorApiRequest, ThermostatVendorApiResponse> create_host(ThermostatManagerController *controller_ptr) {
+    ThermostatHostBuilder builder;
+    builder
+        .set_api_request_parser(ApiRequestParser())
+        .set_api_response_serializer(ApiResponseSerializer())
         .set_raw_data_reader(ThermostatHostBuilder::RawDataReaderInstance(new RawDataReader()))
         .set_raw_data_writer(ThermostatHostBuilder::RawDataWriterInstance(new RawDataWriter()))
-        .set_relay_controller(ThermostatHostBuilder::RelayControllerInstance(new StmRelayController()))
-        .set_scheduler(ThermostatHostBuilder::SchedulerInstance(new StmTimerScheduler()))
-        .set_temp_sensor(ThermostatHostBuilder::SensorInstance(new StmTempSensor()));
-    return s_builder.build();
+        .set_controller(controller_ptr);
+    return builder.build();
 }
