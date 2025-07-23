030b1e822c51e05a246e2db653053e33b30ac33c
fedddot <idanrommm@gmail.com>
2025-07-23 22:37:13 +0000

    updated

--- src/stm_mcu_server.cpp
+++ src/stm_mcu_server.cpp
@@ -2,29 +2,28 @@
 #include <optional>
 #include <stdexcept>
 
-#include "ipc_data.hpp"
+#include "host.hpp"
 #include "ipc_data_writer.hpp"
 #include "ipc_instance.hpp"
+#include "thermostat_manager.hpp"
 #include "thermostat_manager_controller.hpp"
 #include "thermostat_api_request.hpp"
 #include "thermostat_api_response.hpp"
-#include "thermostat_host_builder.hpp"
-#include "proto_thermostat_api_request_parser.hpp"
-#include "proto_thermostat_api_response_serializer.hpp"
+#include "thermostat_vendor.hpp"
 
 using namespace host;
 using namespace vendor;
 using namespace ipc;
 using namespace manager;
 
-static Host<ThermostatVendorApiRequest, ThermostatVendorApiResponse> create_host(ThermostatManagerController *controller_ptr);
+static host::Host<ThermostatVendorApiRequest, ThermostatVendorApiResponse> create_host(ThermostatManagerController *controller_ptr);
 class StmThermoManagerController: public ThermostatManagerController {
 public:
     double read_temperature() const override {
         throw std::runtime_error("NOT IMPLEMENTED");
     }
     void set_relay_state(const bool state) override {
-        throw std::runtime_error("NOT IMPLEMENTED");
+        ;
     }
     manager::Instance<TaskGuard> schedule_task(const Task& task, const std::size_t period_ms) override {
         throw std::runtime_error("NOT IMPLEMENTED");
@@ -35,31 +34,41 @@ int main(void) {
     StmThermoManagerController controller;
     auto host = create_host(&controller);
     while (1) {
-        // host.run_once();
+        host.run_once();
     }
 }
 
-class RawDataReader: public IpcDataReader<RawData> {
+class ApiRequestReader: public ipc::IpcDataReader<ThermostatVendorApiRequest> {
 public:
-    std::optional<ipc::Instance<RawData>> read() override {
+    std::optional<ipc::Instance<ThermostatVendorApiRequest>> read() override {
         return std::nullopt;
     }
 };
 
-class RawDataWriter: public IpcDataWriter<RawData> {
+class ApiResposeWriter: public IpcDataWriter<ThermostatVendorApiResponse> {
 public:
-    void write(const RawData& data) const override {
+    void write(const ThermostatVendorApiResponse& data) const override {
         ;
     }
 };
 
 inline Host<ThermostatVendorApiRequest, ThermostatVendorApiResponse> create_host(ThermostatManagerController *controller_ptr) {
-    ThermostatHostBuilder builder;
-    builder
-        .set_api_request_parser(ApiRequestParser())
-        .set_api_response_serializer(ApiResponseSerializer())
-        .set_raw_data_reader(ThermostatHostBuilder::RawDataReaderInstance(new RawDataReader()))
-        .set_raw_data_writer(ThermostatHostBuilder::RawDataWriterInstance(new RawDataWriter()))
-        .set_controller(controller_ptr);
-    return builder.build();
+    using ThermoHost = Host<ThermostatVendorApiRequest, ThermostatVendorApiResponse>;
+    const auto manager_instance = ThermostatVendor::ThermostatManagerInstance(
+        new ThermostatManager(controller_ptr)
+    );
+    const auto vendor_instance = ThermoHost::VendorInstance(
+        new ThermostatVendor(manager_instance)
+    );
+    return Host<ThermostatVendorApiRequest, ThermostatVendorApiResponse>(
+        ThermoHost::ApiRequestReaderInstance(new ApiRequestReader()),
+        ThermoHost::ApiResponseWriterInstance(new ApiResposeWriter()),
+        vendor_instance,
+        [](const std::exception& e) -> ThermostatVendorApiResponse {
+            return ThermostatVendorApiResponse(
+                ThermostatVendorApiResponse::Result::FAILURE,
+                std::string(e.what())
+            );
+        }
+    );
 }
