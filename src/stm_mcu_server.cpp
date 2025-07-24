c4b4a4015be83c4f53663cbdad3efd39774e8a4e
fedddot <idanrommm@gmail.com>
2025-07-24 17:03:33 +0000

    updated

--- src/stm_mcu_server.cpp
+++ src/stm_mcu_server.cpp
@@ -1,23 +1,19 @@
 #include <cstring>
 #include <optional>
 #include <stdexcept>
+#include <vector>
 
-#include "host.hpp"
-#include "ipc_data_writer.hpp"
-#include "ipc_instance.hpp"
-#include "thermostat_manager.hpp"
-#include "thermostat_manager_controller.hpp"
-#include "thermostat_api_request.hpp"
-#include "thermostat_api_response.hpp"
-#include "thermostat_vendor.hpp"
+#include "thermostat_host_builder.hpp"
+#include "thermostat_controller.hpp"
 
 using namespace host;
-using namespace vendor;
-using namespace ipc;
-using namespace manager;
+using namespace service;
 
-static host::Host<ThermostatVendorApiRequest, ThermostatVendorApiResponse> create_host(ThermostatManagerController *controller_ptr);
-class StmThermoManagerController: public ThermostatManagerController {
+using RawData = std::vector<char>;
+using ApiRequest = ThermostatHostBuilder<RawData>::ApiRequest;
+using ApiResponse = ThermostatHostBuilder<RawData>::ApiResponse;
+
+class StmThermoManagerController: public ThermostatController {
 public:
     double read_temperature() const override {
         return 33.0;
@@ -25,7 +21,7 @@ public:
     void set_relay_state(const bool state) override {
         ;
     }
-    manager::Instance<TaskGuard> schedule_task(const Task& task, const std::size_t period_ms) override {
+    TaskGuard *schedule_task(const Task& task, const std::size_t period_ms) override {
         throw std::runtime_error("NOT IMPLEMENTED");
     }
 };
