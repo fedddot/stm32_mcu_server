7156919cd6123a21fffb5f46d49d849dacccbf54
fedddot <idanrommm@gmail.com>
2025-07-26 17:54:14 +0000

    raw data buff class created

--- include/raw_data_buffer.hpp
+++ include/raw_data_buffer.hpp
@@ -4,29 +4,62 @@
 #include <array>
 #include <cstddef>
 #include <stdexcept>
-#include <vector>
 
 namespace service {
 	template <std::size_t N>
 	class RawDataBuffer {
 	public:
-		RawDataBuffer() {}
+		RawDataBuffer() : m_head(0), m_tail(0), m_full(false) {}
 		RawDataBuffer(const RawDataBuffer&) = delete;
 		RawDataBuffer& operator=(const RawDataBuffer&) = delete;
 		virtual ~RawDataBuffer() noexcept = default;
-		
-		void push_back(const char ch) {
-			
-		}
-		char pop_first() {
 
+		void push_back(const char ch) {
+			if (m_full) {
+				throw std::overflow_error("RawDataBuffer is full");
+			}
+			m_buffer[m_tail] = ch;
+			m_tail = (m_tail + 1) % N;
+			if (m_tail == m_head) {
+				m_full = true;
+			}
 		}
+
+		char pop_first() {
+			if (empty()) {
+				throw std::underflow_error("RawDataBuffer is empty");
+			}
+			char value = m_buffer[m_head];
+			m_head = (m_head + 1) % N;
+			m_full = false;
+			return value;
+		}
+
 		std::size_t size() const {
+			if (m_full) {
+				return N;
+			}
+			if (m_tail >= m_head) {
+				return m_tail - m_head;
+			} else {
+				return N - m_head + m_tail;
+			}
 		}
+
 		void clear() {
+			m_head = 0;
+			m_tail = 0;
+			m_full = false;
+		}
+
+		bool empty() const {
+			return (!m_full && (m_head == m_tail));
 		}
 	private:
 		std::array<char, N> m_buffer;
+		std::size_t m_head;
+		std::size_t m_tail;
+		bool m_full;
 	};
 }
 
