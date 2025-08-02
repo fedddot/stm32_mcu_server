f0feeb049681412bca8a97497b1180423604ef26
fedddot <idanrommm@gmail.com>
2025-08-02 17:07:38 +0000

    guard added

--- include/stm_thermo_manager_controller.hpp
+++ include/stm_thermo_manager_controller.hpp
@@ -1,8 +1,7 @@
 #ifndef STM_THERMO_MANAGER_CONTROLLER_HPP
 #define STM_THERMO_MANAGER_CONTROLLER_HPP
 
-#include <stdexcept>
-
+#include "stm_timer_controller.hpp"
 #include "thermostat_controller.hpp"
 
 namespace stm32 {
@@ -15,8 +14,21 @@ namespace stm32 {
             ;
         }
         TaskGuard *schedule_task(const Task& task, const std::size_t period_ms) override {
-            throw std::runtime_error("NOT IMPLEMENTED");
+            m_timer_controller.schedule_task(task, period_ms);
+            return new TimerTaskGuard(&m_timer_controller);
         }
+    private:
+        StmTimerController m_timer_controller;
+
+        class TimerTaskGuard: public TaskGuard {
+        public:
+            TimerTaskGuard(StmTimerController *timer_controller_ptr) : m_timer_controller_ptr(timer_controller_ptr) {}
+            void unschedule() override {
+                m_timer_controller_ptr->unschedule_task();
+            }
+        private:
+            StmTimerController *m_timer_controller_ptr;
+        };
     };
 }
 
