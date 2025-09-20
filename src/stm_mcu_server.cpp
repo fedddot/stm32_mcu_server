367550a6eb12342448b250a6044bb10f0a4bc2b2
fedddot <idanrommm@gmail.com>
2025-09-20 14:22:23 +0000

    updated

--- src/stm_mcu_server.cpp
+++ src/stm_mcu_server.cpp
@@ -1,12 +1,23 @@
 #include <cstdint>
+#include <stdexcept>
 
+#include "api_message_writer.hpp"
+#include "package_writer.hpp"
 #include "ring_buffer_input_stream.hpp"
+#include "stepper_request.hpp"
+#include "stepper_response.hpp"
 #include "stm_uart_controller.hpp"
 #include "stepper_service.hpp"
+#include "package_reader.hpp"
+#include "package_utils.hpp"
+#include "api_message_reader.hpp"
+#include "host.hpp"
 
 #include "stm32f103xb.h"
 
+using namespace host;
 using namespace service;
+using namespace ipc;
 
 #ifndef DATA_BUFFER_SIZE
 #  error "DATA_BUFFER_SIZE is not defined"
@@ -21,9 +32,57 @@ static void init_clock();
 
 int main(void) {
     init_clock();
+    StmUartController uart_controller(
+        [buff_ptr = &s_buffer](const std::uint8_t byte) {
+            buff_ptr->enqueue(byte);
+        }
+    );
+    PackageReader package_reader(
+        &s_buffer,
+        [](const InputStream<std::uint8_t>& stream) -> std::size_t {
+            std::vector<std::uint8_t> package_size_data(PACKAGE_HEADER_LENGTH, 0);
+            for (std::size_t i = 0; i < PACKAGE_HEADER_LENGTH; ++i) {
+                package_size_data[i] = stream.inspect(i);
+            }
+            return parse_package_size(package_size_data);
+        },
+        PACKAGE_HEADER_LENGTH
+    );
+    PackageWriter package_writer(
+        [](const std::vector<std::uint8_t>& data, const std::size_t& header_size) {
+            return serialize_package_size(data.size(), PACKAGE_HEADER_LENGTH);
+        },
+        [uart_controller_ptr = &uart_controller](const std::vector<std::uint8_t>& data) {
+            uart_controller_ptr->write(data);
+        },
+        PACKAGE_HEADER_LENGTH
+    );
+    ApiMessageReader<StepperRequest> request_reader(
+        &package_reader,
+        [](const std::vector<std::uint8_t>& data) -> StepperRequest {
+            throw std::runtime_error("not implemented");
+        }
+    );
+    ApiMessageWriter<StepperResponse> response_writer(
+        &package_writer,
+        [](const StepperResponse& request) -> std::vector<std::uint8_t> {
+            throw std::runtime_error("not implemented");
+        }
+    );
     StepperService service;
+    Host<StepperRequest, StepperResponse> host(
+        &request_reader,
+        &response_writer,
+        &service,
+        [](const std::exception& e) {
+            return StepperResponse(
+                StepperResponse::Result::FAILURE,
+                std::string(e.what())
+            );
+        }
+    );
     while (true) {
-        // app.run_once();
+        host.run_once();
     }
 }
 
