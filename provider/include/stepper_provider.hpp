7bd68205029673ac187864e785ff597d7a13b61f
fedddot <idanrommm@gmail.com>
2025-09-20 15:40:04 +0000

    stepper_provider added

---
+++ provider/include/stepper_provider.hpp
@@ -0,0 +1,12 @@
+#ifndef	STEPPER_PROVIDER_HPP
+#define	STEPPER_PROVIDER_HPP
+
+#include "provider.hpp"
+
+namespace provider {
+	class StepperProvider: public Provider {
+	public:
+	};
+}
+
+#endif // STEPPER_PROVIDER_HPP
--- service/include/stepper_service.hpp
+++ service/include/stepper_service.hpp
@@ -6,13 +6,21 @@
 #include "service.hpp"
 #include "stepper_request.hpp"
 #include "stepper_response.hpp"
+#include "stepper_provider.hpp"
 
 namespace service {
 	class StepperService: public Service<StepperRequest, StepperResponse> {
 	public:
+		StepperService(provider::StepperProvider *provider): m_provider(provider) {
+			if (!m_provider) {
+				throw std::invalid_argument("null provider ptr received");
+			}
+		}
 		StepperResponse run_api_request(const StepperRequest& request) override {
 			throw std::runtime_error("not implemented");
 		}
+	private:
+		provider::StepperProvider *m_provider;
 	};
 }
 
