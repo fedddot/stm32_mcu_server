e37bda59803c864de7e329adde14ac720a01e30d
fedddot <idanrommm@gmail.com>
2025-09-20 12:03:42 +0000

    stepper_response added

---
+++ service/include/stepper_response.hpp
@@ -0,0 +1,37 @@
+#ifndef	STEPPER_RESPONSE_HPP
+#define	STEPPER_RESPONSE_HPP
+
+#include <optional>
+
+#include "stepper_types.hpp"
+
+namespace service {
+	class StepperResponse {
+	public:
+		enum class Result: int {
+			SUCCESS,
+			FAILURE,
+		};
+		StepperResponse(
+			const Result& result,
+			const std::optional<State>& state = std::nullopt
+		): m_result(result), m_state(state) {
+
+		}
+		StepperResponse(const StepperResponse&) = default;
+		StepperResponse& operator=(const StepperResponse&) = delete;
+		virtual ~StepperResponse() noexcept = default;
+
+		Result result() const {
+			return m_result;
+		}
+		std::optional<State> state() const {
+			return m_state;
+		}
+	private:
+		const Result m_result;
+		const std::optional<State> m_state;
+	};
+}
+
+#endif // STEPPER_RESPONSE_HPP
