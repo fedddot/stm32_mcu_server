230e140841ff698337dc132cddd7ebc5fecd63a3
fedddot <idanrommm@gmail.com>
2025-09-20 15:30:32 +0000

    isr vector returned

---
+++ include/stm_isr_vector.hpp
@@ -0,0 +1,15 @@
+#ifndef STM_ISR_VECTOR_HPP
+#define STM_ISR_VECTOR_HPP
+
+#include <functional>
+
+namespace stm32 {
+    using IsrCallback = std::function<void(void)>;
+
+    void init_hard_fault_isr(const IsrCallback& callback);
+    void init_nmi_isr(const IsrCallback& callback);
+    void init_uart1_isr(const IsrCallback& callback);
+    void init_timer1_up_isr(const IsrCallback& callback);
+}
+
+#endif // STM_ISR_VECTOR_HPP
