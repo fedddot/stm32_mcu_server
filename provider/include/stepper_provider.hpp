5bc954539b217401577e9001abc96239acfc67a7
fedddot <idanrommm@gmail.com>
2025-09-23 14:30:19 +0300

    stepper provider added

--- provider/include/stepper_provider.hpp
+++ provider/include/stepper_provider.hpp
@@ -2,10 +2,16 @@
 #define	STEPPER_PROVIDER_HPP
 
 #include "provider.hpp"
+#include "stepper_types.hpp"
 
 namespace provider {
 	class StepperProvider: public Provider {
 	public:
+		virtual ~StepperProvider() noexcept = default;
+		virtual void enable() = 0;
+		virtual void disable() = 0;
+		virtual void step(const int steps_number, const service::Direction direction, const service::Duration step_duration) = 0;
+		virtual service::State status() const = 0;
 	};
 }
 
